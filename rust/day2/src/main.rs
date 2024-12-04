37d33b2b80e93e914b9c0bf3d8c4025a3427301d
hesom <sommerhoff.hendrik@gmail.com>
2024-12-05 00:08:00 +0100

    Refactoring

--- rust/day2/src/main.rs
+++ rust/day2/src/main.rs
@@ -1,5 +1,30 @@
 use std::fs;
 
+fn safe(nums: &Vec<i32>) -> bool {
+    let n = nums.len();
+
+    let mut neg_cnt = 0;
+    let mut pos_cnt = 0;
+    let mut valid_cnt = 0;
+
+    for i in 1..n {
+        let diff = nums[i] - nums[i - 1];
+
+        if diff < 0 {
+            neg_cnt += 1;
+        }
+        if diff > 0 {
+            pos_cnt += 1;
+        }
+
+        if diff.abs() > 0 && diff.abs() < 4 && (pos_cnt == i || neg_cnt == i) {
+            valid_cnt += 1;
+        }
+    }
+
+    valid_cnt == n as i32 - 1
+}
+
 fn main() {
     let content = fs::read_to_string("../inputs/day2.input").unwrap();
 
@@ -7,30 +32,8 @@ fn main() {
     let mut part2 = 0;
     for line in content.lines() {
         let nums: Vec<i32> = line.split(" ").map(|x| x.parse().unwrap()).collect();
-        let n = nums.len();
 
-        let mut neg_cnt = 0;
-        let mut pos_cnt = 0;
-        let mut valid_cnt = 0;
-
-        for i in 1..n {
-            let diff = nums[i] - nums[i - 1];
-
-            if diff < 0 {
-                neg_cnt += 1;
-            }
-            if diff > 0 {
-                pos_cnt += 1;
-            }
-
-            if diff.abs() > 0 && diff.abs() < 4 && (pos_cnt == i || neg_cnt == i) {
-                valid_cnt += 1;
-            }
-        }
-
-        let n = n as i32 - 1;
-
-        if valid_cnt == n {
+        if safe(&nums) {
             part1 += 1;
             part2 += 1;
         } else {
@@ -38,26 +41,7 @@ fn main() {
             for to_remove in 0..nums.len() {
                 let mut nums = nums.clone();
                 nums.remove(to_remove);
-                let n = nums.len();
-                let mut neg_cnt = 0;
-                let mut pos_cnt = 0;
-                let mut valid_cnt = 0;
-                for i in 1..n {
-                    let diff = nums[i] - nums[i - 1];
-
-                    if diff < 0 {
-                        neg_cnt += 1;
-                    }
-                    if diff > 0 {
-                        pos_cnt += 1;
-                    }
-
-                    if diff.abs() > 0 && diff.abs() < 4 && (pos_cnt == i || neg_cnt == i) {
-                        valid_cnt += 1;
-                    }
-                }
-
-                if valid_cnt == n as i32 - 1 {
+                if safe(&nums) {
                     success = true;
                     break;
                 }
