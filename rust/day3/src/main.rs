508c554ee3e87e815d741c4e1bf4022cfc544175
hesom <sommerhoff.hendrik@gmail.com>
2024-12-11 01:24:09 +0100

    Day 3 part 2 in Rust

--- rust/day3/src/main.rs
+++ rust/day3/src/main.rs
@@ -1,27 +1,38 @@
+use regex::Regex;
 use std::fs;
 
 fn main() {
     let content = fs::read_to_string("../inputs/day3.input").unwrap();
 
-    let mut l = 0;
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    let part1 = re
+        .captures_iter(&content)
+        .map(|caps| {
+            let (_, [val1, val2]) = caps.extract();
+            (val1.parse::<i32>().unwrap(), val2.parse::<i32>().unwrap())
+        })
+        .fold(0, |acc, (val1, val2)| acc + (val1 * val2));
 
-    let mut res = 0;
-    while let Some(idx) = content[l..].find("mul") {
-        let leftp = l + idx + 3;
-        if content.chars().nth(leftp).unwrap_or(' ') == '(' {
-            if let Some(rightp) = content[leftp..].find(")") {
-                let vals: Vec<_> = content[leftp+1..leftp+rightp].split(",").collect();
-                if vals.len() == 2 {
-                    if let Ok(a) = vals[0].parse::<i32>() {
-                        if let Ok(b) = vals[1].parse::<i32>() {
-                            res += a * b;
-                        }
-                    };
-                }
-            };
+    let re = Regex::new(r"mul\((\d+),(\d+)\)|(do\(\))|(don't\(\))").unwrap();
+    let mut part2 = 0;
+    let mut enabled = true;
+    for caps in re.captures_iter(&content) {
+        if let Some(_) = caps.get(3) {
+            enabled = true;
+            continue;
         }
-        l += idx + 1;
+        if let Some(_) = caps.get(4) {
+            enabled = false;
+            continue;
+        }
+
+        if enabled {
+            let val1 = caps.get(1).unwrap().as_str().parse::<i32>().unwrap();
+            let val2 = caps.get(2).unwrap().as_str().parse::<i32>().unwrap();
+            part2 += val1 * val2;
+        }
     }
 
-    println!("Part 1: {}", res);
+    println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
 }
